63a4368410a94b7525217943af1bf943fba3ce82
Ryan Travitz <rtravitz@gmail.com>
2021-01-01 18:21:42 -0800

    Change to async/await for lambda

--- src/api/vessels.js
+++ src/api/vessels.js
@@ -1,17 +1,16 @@
 const axios = require('axios')
 
-exports.handler = () => {
-  axios.get('https://www.wsdot.com/Ferries/VesselWatch/Vessels.ashx')
-    .then((res) => (
-      {
-        statusCode: 200,
-        body: res.data,
-      }
-    ))
-    .catch((err) => (
-      {
-        statusCode: 500,
-        body: err.toString(),
-      }
-    ))
+exports.handler = async () => {
+  try {
+    const res = await axios.get('https://www.wsdot.com/Ferries/VesselWatch/Vessels.ashx')
+    return {
+      statusCode: 200,
+      body: res.data,
+    }
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: err.toString(),
+    }
+  }
 }
