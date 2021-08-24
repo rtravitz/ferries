0eab60b62c3b8e0ca2f7240d5fab5ab7beb6060e
Ryan Travitz <rtravitz@gmail.com>
2021-08-24 16:48:59 -0700

    Update error message position

--- src/components/FetchError.js
+++ src/components/FetchError.js
@@ -16,13 +16,13 @@ export default function FetchError({ active }) {
     >
       <div
         ref={nodeRef}
-        className="flex flex-col fixed top-4 left-4 z-50 bg-gray-200 rounded-lg justify-center items-center w-1/2 shadow-lg"
+        className="fixed inset-x-0 inset-y-1/2 w-3/4 mx-auto z-50 flex flex-col justify-center items-center rounded-lg shadow-lg"
       >
         <div className="w-full rounded-t-lg py-1 flex items-end bg-red-700">
           <img className="h-6 mx-2" src={exclamation} alt="exclamation mark in a circle" />
           <h3 className="text-gray-200 text-xl font-bold leading-none">Uh oh...</h3>
         </div>
-        <p className="text-red-700 p-2">There was an error fetching updated vessel information.</p>
+        <p className="text-red-700 bg-gray-200 rounded-b-lg p-2">There was an error fetching updated vessel information. You can refresh or try again later.</p>
       </div>
     </CSSTransition>
   )
