35502f5aad3b74a8a98f987dd2dfaf96249a112f
Ryan Travitz <rtravitz@gmail.com>
2021-08-24 16:54:57 -0700

    Make error text match header size

--- src/components/FetchError.js
+++ src/components/FetchError.js
@@ -16,13 +16,13 @@ export default function FetchError({ active }) {
     >
       <div
         ref={nodeRef}
-        className="fixed inset-x-0 inset-y-1/2 w-3/4 mx-auto z-50 flex flex-col justify-center items-center rounded-lg shadow-lg"
+        className="fixed inset-x-0 inset-y-1/2 lg:w-1/4 w-3/4 mx-auto z-50 flex flex-col justify-center items-center rounded-lg shadow-lg"
       >
         <div className="w-full rounded-t-lg py-1 flex items-end bg-red-700">
           <img className="h-6 mx-2" src={exclamation} alt="exclamation mark in a circle" />
           <h3 className="text-gray-200 text-xl font-bold leading-none">Uh oh...</h3>
         </div>
-        <p className="text-red-700 bg-gray-200 rounded-b-lg p-2">There was an error fetching updated vessel information. You can refresh or try again later.</p>
+        <p className="text-red-700 w-full bg-gray-200 rounded-b-lg p-2">There was an error fetching updated vessel information. You can refresh or try again later.</p>
       </div>
     </CSSTransition>
   )
