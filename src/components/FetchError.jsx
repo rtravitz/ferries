83356b42fce04508c30916d26d13c048be9abdb8
Ryan Travitz <rtravitz@gmail.com>
2021-09-28 19:16:55 -0700

    Change away from react-transition-group Update axios minor bump

--- src/components/FetchError.jsx
+++ src/components/FetchError.jsx
@@ -1,29 +1,24 @@
-import React, { useRef } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import React from 'react'
 import exclamation from '../assets/exclamation-outline.svg'
+import { useTransition, config, a } from '@react-spring/web'
 
 export default function FetchError({ active }) {
-  const nodeRef = useRef(null)
+  const transition = useTransition(active, {
+    from: { opacity: 0, y: -30 },
+    enter: { opacity: 1, y: 0 },
+    leave: { opacity: 0, y: -30 },
+    config: config.molasses
+  })
 
   return (
-    <CSSTransition
-      // nodeRef explanation: https://github.com/reactjs/react-transition-group/issues/668#issuecomment-695162879
-      nodeRef={nodeRef}
-      in={active}
-      timeout={1000}
-      classNames="alert"
-      unmountOnExit
-    >
-      <div
-        ref={nodeRef}
-        className="flex flex-col fixed top-4 left-4 z-50 bg-gray-200 rounded-lg justify-center items-center w-1/2 shadow-lg"
-      >
+    transition((styles, item) => item && (
+      <a.div className="fixed inset-x-0 inset-y-1/2 lg:w-1/4 w-3/4 mx-auto z-50 flex flex-col justify-center items-center rounded-lg shadow-lg" style={styles} >
         <div className="w-full rounded-t-lg py-1 flex items-end bg-red-700">
           <img className="h-6 mx-2" src={exclamation} alt="exclamation mark in a circle" />
           <h3 className="text-gray-200 text-xl font-bold leading-none">Uh oh...</h3>
         </div>
-        <p className="text-red-700 p-2">There was an error fetching updated vessel information.</p>
-      </div>
-    </CSSTransition>
+        <p className="text-red-700 w-full bg-gray-200 rounded-b-lg p-2">There was an error fetching updated vessel information.</p>
+      </a.div>
+    ))
   )
 }
