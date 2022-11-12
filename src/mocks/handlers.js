0ef10e5ac17a1107618c814cc7b9ab38eec12dbb
Ryan Travitz <rtravitz@gmail.com>
2022-11-12 12:10:09 -0800

    Upgrade deps and add delay to mock handler

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3,6 +3,10 @@ import vesselsFixture from './fixtures/vessels.json'
 
 export const handlers = [
   rest.get('http://localhost:5273/ferries', (req, res, ctx) => {
-    return res(ctx.json(vesselsFixture), ctx.set('Access-Control-Allow-Origin', '*'))
+    return res(
+      ctx.delay(1000),
+      ctx.json(vesselsFixture), 
+      ctx.set('Access-Control-Allow-Origin', '*')
+    )
   }),
 ]
