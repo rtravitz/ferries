9abc7608e17a8b5401368cfc8320dd4f02a8cb8f
Ryan Travitz <rtravitz@gmail.com>
2022-11-16 18:48:21 -0800

    Update toggle switch color

--- src/components/SettingsView.jsx
+++ src/components/SettingsView.jsx
@@ -12,8 +12,8 @@ export default function SettingsView({ showOutOfService, setShowOutOfService }) 
               defaultChecked={showOutOfService}
               id="outOfServiceToggle"
               className="sr-only" />
-            <div className="block bg-gray-600 w-14 h-8 rounded-full" />
-            <div className="toggle-dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition" />
+            <div className="block bg-gray-400 w-14 h-8 rounded-full shadow" />
+            <div className="toggle-dot absolute left-1 top-1 bg-gray-200 w-6 h-6 rounded-full transition" />
           </div>
           <div className="ml-3 text-gray-700 font-medium">Show out of service vessels</div>
         </label>
